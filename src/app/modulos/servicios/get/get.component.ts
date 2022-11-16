498dbe028919c4c1ad7e958848d0f4c4d37bb45f
cfp96 <carfelcoper@gmail.com>
2022-11-15 21:56:38 -0500

    mejora en la lista de los servicios para no mostrar ids

--- src/app/modulos/servicios/get/get.component.ts
+++ src/app/modulos/servicios/get/get.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ClienteModel } from 'src/app/modelos/cliente.model';
+import { EncomiendaModel } from 'src/app/modelos/encomienda.model';
 import { ServicioModel } from 'src/app/modelos/servicio.model';
+import { ClienteService } from 'src/app/servicios/cliente.service';
+import { EncomiendaService } from 'src/app/servicios/encomienda.service';
 import { ServicioService } from 'src/app/servicios/servicio.service';
 import Swal from 'sweetalert2'
 
@@ -10,12 +14,26 @@ import Swal from 'sweetalert2'
 })
 export class GetComponent implements OnInit {
 
-  constructor(private servicioService: ServicioService) { }
+  constructor(private servicioService: ServicioService, private clienteService: ClienteService, private encomiendaService: EncomiendaService) { }
 
   listado: ServicioModel[] = []
 
   getAll(){
     this.servicioService.getAll().subscribe((data: ServicioModel[]) => {
+      let clienteOrigen = this.clienteService.getWithId(String(data[0]['origen'])).subscribe((origenData: ClienteModel) => {
+        console.log(origenData)
+        data[0]['origen'] = String(origenData.departamento) + ',' + String(origenData.ciudad);      
+      });
+      let clienteDestino = this.clienteService.getWithId(String(data[0]['destino'])).subscribe((destinoData: ClienteModel) => {
+        console.log(destinoData)
+        data[0]['destino'] = String(destinoData.departamento) + ',' + String(destinoData.ciudad);      
+      });
+      
+      let Encomineda = this.encomiendaService.getWithId(String(data[0]['encomienda'])).subscribe((encomiendaData: EncomiendaModel) => {
+        console.log(encomiendaData)
+        data[0]['encomienda'] = 'Encomienda de tipo: ' + String(encomiendaData.tipo);      
+      });
+
       this.listado = data
       console.log(data)
     })
