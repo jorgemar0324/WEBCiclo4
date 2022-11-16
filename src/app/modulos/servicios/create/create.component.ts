bbb0c04e949bcd1482f66c859bd21d27c34a12d7
cfp96 <carfelcoper@gmail.com>
2022-11-15 20:55:55 -0500

    agregada vista de listado y creacion de servicios

--- src/app/modulos/servicios/create/create.component.ts
+++ src/app/modulos/servicios/create/create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ServicioModel } from 'src/app/modelos/servicio.model';
+import { ServicioService } from 'src/app/servicios/servicio.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-create',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private servicioService: ServicioService,
+    private router: Router
+) { }
 
-  ngOnInit(): void {
-  }
+fgValidacion = this.fb.group({
+  fecha: ['', [Validators.required]],
+  valor: ['', [Validators.required]],
+  origen: ['', [Validators.required]],
+  destino: ['', [Validators.required]],
+  encomienda: ['', [Validators.required]]
+});
+
+store(){
+  let servicio = new ServicioModel();
+  servicio.fecha = this.fgValidacion.controls["fecha"].value as unknown as Date;
+  servicio.valor = this.fgValidacion.controls["valor"].value as string;
+  servicio.origen = this.fgValidacion.controls["origen"].value as string;
+  servicio.destino = this.fgValidacion.controls["destino"].value as string;
+  servicio.encomienda = this.fgValidacion.controls["encomienda"].value as string;
+
+  this.servicioService.store(servicio).subscribe((data: ServicioModel)=> {
+    Swal.fire('Creado correctamente!', '', 'success')
+    this.router.navigate(['/servicios/get']);
+  },
+  (error: any) => {
+    console.log(error)
+    alert("Error en el envio");
+  })
+}
+
+
+ngOnInit(): void {
+}
 
 }
